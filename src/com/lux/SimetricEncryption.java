436db605784b9b57f6a4897911b2f001bed0415a
mastersilvapt <andresilva.aer@gmail.com>
2020-08-13 17:08:14 +0100

    Fix Desencryption error / Changing IvParameterSpec to GCMParameterSpec

--- src/com/lux/SimetricEncryption.java
+++ src/com/lux/SimetricEncryption.java
@@ -19,7 +19,6 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.GCMParameterSpec;
-import javax.crypto.spec.IvParameterSpec;
 
 public class Other {
 
