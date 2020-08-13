ac6eebed2bbe3021e7b4e90f72c50adf987713ba
mastersilvapt <andresilva.aer@gmail.com>
2020-08-13 17:07:58 +0100

    Fix Desencryption error / Changing IvParameterSpec to GCMParameterSpec

--- src/com/lux/SimetricEncryption.java
+++ src/com/lux/SimetricEncryption.java
@@ -1,32 +1,32 @@
-/* 
+/*
 Third draft for Simetric Type Encryption
 	- Encrypton seems to work fine, but can be improved and Block cipher mode of operation, Counter Mode, Padding are to be
 	studied
-	- Decryption doesnt work
 */
 
 
-package src.com.lux;
+//package src.com.lux;
 
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
-import javax.crypto.SecretKey; 
+import javax.crypto.SecretKey;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.IvParameterSpec;
 
-public class SimetricEncryption {
+public class Other {
 
     private static final String ALGORITHM = "AES";
     private static final String AES_CIPHER_ALGORITHM = "AES/GCM/NoPadding";
 
-    private SimetricEncryption() {
+    private Other() {
     }
 
     public static byte[] createInitializationVector() {
@@ -40,52 +40,54 @@ public class SimetricEncryption {
     public static byte[] doAESEncryption(String plainText, SecretKey secretKey, byte[] initializationVector) {
 
         Cipher cipher = null;
-		try {
-			cipher = Cipher.getInstance(AES_CIPHER_ALGORITHM);
-		} catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
-			e.printStackTrace();
-		}
-		
-        //IvParameterSpec ivParameterSpec = new IvParameterSpec(initializationVector);
+        try {
+            cipher = Cipher.getInstance(AES_CIPHER_ALGORITHM);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            e.printStackTrace();
+        }
+
+        GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(128,initializationVector);
 
         try {
-			cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-		} catch (InvalidKeyException e) {
-			e.printStackTrace();
-		}
+            assert cipher != null;
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey,gcmParameterSpec);
+        } catch (InvalidKeyException | InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        }
 
         try {
-			return cipher.doFinal(plainText.getBytes());
-		} catch (IllegalBlockSizeException | BadPaddingException e) {
-			e.printStackTrace();
-			return initializationVector;
-		}
+            return cipher.doFinal(plainText.getBytes());
+        } catch (IllegalBlockSizeException | BadPaddingException e) {
+            e.printStackTrace();
+            return initializationVector;
+        }
     }
 
     public static String doAESDecryption(byte[] cipherText, SecretKey secretKey, byte[] initializationVector) {
 
-		Cipher cipher = null;
-		
-		try {
-			cipher = Cipher.getInstance(AES_CIPHER_ALGORITHM);
-		} catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
-			e.printStackTrace();
-		}
-
-        // IvParameterSpec ivParameterSpec = new IvParameterSpec( initializationVector);
+        Cipher cipher = null;
 
         try {
-			cipher.init(Cipher.DECRYPT_MODE, secretKey);
-		} catch (InvalidKeyException e) {
-			e.printStackTrace();
-		} 
+            cipher = Cipher.getInstance(AES_CIPHER_ALGORITHM);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            e.printStackTrace();
+        }
+
+        GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(128,initializationVector);
+
+        try {
+            assert cipher != null;
+            cipher.init(Cipher.DECRYPT_MODE, secretKey,gcmParameterSpec);
+        } catch (InvalidKeyException | InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        }
 
         byte[] result = new byte[16];
-		try {
-			result = cipher.doFinal(cipherText);
-		} catch (IllegalBlockSizeException | BadPaddingException e) {
-			e.printStackTrace();
-		}
+        try {
+            result = cipher.doFinal(cipherText);
+        } catch (IllegalBlockSizeException | BadPaddingException e) {
+            e.printStackTrace();
+        }
 
         return new String(result);
     }
@@ -93,13 +95,13 @@ public class SimetricEncryption {
     public static SecretKey createAESKey() {
         SecureRandom securerandom = new SecureRandom();
 
-		KeyGenerator keygenerator;
-		SecretKey key = null; 		// TODO: Possible vulnerability in the event of Algorithm Error, the SecretKey gets forced to null
+        KeyGenerator keygenerator;
+        SecretKey key = null; 		// TODO: Possible vulnerability in the event of Algorithm Error, the SecretKey gets forced to null
         try {
 
-			keygenerator = KeyGenerator.getInstance(ALGORITHM);
-			keygenerator.init(256, securerandom);
-			key = keygenerator.generateKey();
+            keygenerator = KeyGenerator.getInstance(ALGORITHM);
+            keygenerator.init(256, securerandom);
+            key = keygenerator.generateKey();
 
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
